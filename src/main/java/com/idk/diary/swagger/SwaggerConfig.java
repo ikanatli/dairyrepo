7a2b81d865f7a0b3cc9baa58c32a4e2d5b79bb78
ikanatli <ikanatli@yahoo.com>
2022-10-10 17:24:54 +0300

    Swagger Configuration Customized.

--- src/main/java/com/idk/diary/swagger/SwaggerConfig.java
+++ src/main/java/com/idk/diary/swagger/SwaggerConfig.java
@@ -2,25 +2,63 @@ package com.idk.diary.swagger;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.time.OffsetDateTime;
+import java.util.Date;
 
 
 /*
    Swagger URL:  http://localhost:8080/swagger-ui/
  */
-@Configuration
-public class SwaggerConfig {
+@Configuration("DiarySwaggerDocumentationConfig")
+public class SwaggerConfig implements WebMvcConfigurer {
+
+
 
     @Bean
     public Docket api(){
         return new Docket(DocumentationType.SWAGGER_2)
                 .select() // provides a way to control the endpoints exposed by Swagger.
-                .apis(RequestHandlerSelectors.any()) // Using any() make documentation for our entire API available through Swagger.
-                .paths(PathSelectors.any()) // Using any() make documentation for our entire API available through Swagger.
+                .apis(RequestHandlerSelectors.basePackage("com.idk.diary"))
+                .build()
+                .ignoredParameterTypes(Throwable.class, StackTraceElement.class)
+                //.directModelSubstitute(OffsetDateTime.class, Date.class)
+                .apiInfo(apiInfo());
+    }
+
+    ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Diary App Service")
+                .description("This SwaggerUI environment generated for the Diary Application Service.")
+                .license("Apache License Version 2.0")
+                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0")
+                .version("1.0.0")
+                .contact(new Contact("ilker danyal kanatli", "", "ikanatli@yahoo.com"))
                 .build();
     }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry){
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/schemas/**")
+                .addResourceLocations("classpath:/static/schemas/");
+    }
+
+
 
 }
